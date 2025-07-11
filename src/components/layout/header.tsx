6f2441b1d2c66cc3bea6284949611ee22b5089ab
rob <heidlessemail03@gmail.com>
2025-07-11 09:40:53 +0000

    Responsive site: 	When the viewport width is less than 900px: 		The sect

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -60,7 +60,7 @@ export function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
   const pathname = usePathname();
-  const isDesktop = useBreakpoint(1000);
+  const isDesktop = useBreakpoint(900);
 
   useEffect(() => {
     setIsMounted(true);
