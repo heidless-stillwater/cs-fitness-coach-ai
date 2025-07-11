fecd2468bdd80521aa0447440ea63fe757a03991
rob <heidlessemail03@gmail.com>
2025-07-11 11:43:02 +0000

    # TEAL Accent Primary color: #3F51B5 Secondary color: #FFBF69 Backgroun

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Montserrat', 'sans-serif'],
-        headline: ['Open Sans', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
