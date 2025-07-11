651c17380bccfee2b79478f090d46f5e5f4f5858
rob <heidlessemail03@gmail.com>
2025-07-11 09:39:09 +0000

    Responsive site: 	When the viewport width is less than 1000px: 		The sec

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -60,7 +60,7 @@ export function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
   const pathname = usePathname();
-  const isDesktop = useBreakpoint(1200);
+  const isDesktop = useBreakpoint(1000);
 
   useEffect(() => {
     setIsMounted(true);
